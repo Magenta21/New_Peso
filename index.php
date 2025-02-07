ff2b3be94316b71615ed9b914314d2df7f80a8c2
Magenta21 <regeniw341@mcatag.com>
2025-02-07 14:29:31 +0800

    home1

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Layout Design</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <img src="img/logolb.png" alt="lblogo" style="height: 50px;">
+                </div>
+                <div class="col-md-8">
+                    <h3 style="margin-top: 5px; font-weight: 900; color: #ffffff;">MUNICIPALITY OF LOS BANOS</h3>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <span class="navbar-text text-white w-100 text-center">
+                <button class="dropbtn"> Home </button>
+                <div class="dropdown">
+                <button class="dropbtn">Applicant</button>
+                <div class="dropdown-content">
+                <a href="#">training</a>
+                <a href="#">Employment</a>
+                </div>
+              </div>
+              <div class="dropdown">
+                <button class="dropbtn">Employer</button>
+                <div class="dropdown-content">
+                <a href="#">Direct Hire</a>
+                <a href="#">Local Hire</a>
+                <a href="#">Local LB</a>
+                <a href="#">Overseas</a>
+                </div>
+              </div>
+              <button class="dropbtn"> News </button>
+              <button class="dropbtn"> Educational Institutions </button>
+              <button class="dropbtn"> Jobs </button>
+            </span>
+        </div>
+    </nav>
+    <div class="banner">
+    </div>
+
+    <div class="container mt-4">
+        <div class="row mb-4 text-center">
+            <div class="col-8 text-center  text-black py-3 border border-5 border-danger p-5 rounded-5">
+                It is with great pride and commitment that we introduce the newly developed Public Employment Service Office (PESO) website, a vital step toward enhancing employment opportunities for our community. This platform is designed to connect job seekers with employers, provide access to training programs, and offer essential career guidance to empower our local workforce. As we strive for progress and inclusive growth, this digital initiative ensures that every resident of Los Baños has an accessible, transparent, and efficient employment service. Together, we build a future where opportunities are within reach for all.
+            </div>
+            <div class="col-md-4">
+                <img src="img/mayor.jpg" alt="Sample Image" class="img-fluid  rounded-circle" style="width: 200px; height: 200px;">
+            </div>
+        </div>
+        
+        <div class="col mb-3 justify-content-center">
+        </div>
+
+        <div class="row mb-4 text-center">
+            <div class="col-md-4">
+                <div class="profile">
+                    <img src="img/mission.png" alt="Profile 1" class="img-fluid" style="width: 100px; height: 100px;">
+                    <div><br><h3>Mission<h3></div>
+                    <div>To carry out full employment and equality of employment opportunities for all, and for this purpose, strengthen and expand the existing employment facilitation service machinery.</div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="profile">
+                    <img src="img/vision.png" alt="Profile 2" class="img-fluid" style="width: 100px; height: 100px;">
+                    <div><br><h3>Vission<h3></div>
+                    <div>HANAPBUHAY PARA SA LAHAT TUNGO SA MAUNLAD, MASAGANA AT MASAYANG LOS BAÑOS</div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="profile">
+                    <img src="img/values.png" alt="Profile 3" class="img-fluid" style="width: 100px; height: 100px;">
+                    <div><br><h3>Values<h3></div>
+                    <div>Integrity / Honesty / Teamwork
+                        Innovation / Cooperation / Diversity / Trust
+                        Passion / Respect / Accountability</div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="block mb-4">
+                    <h3> ABOUT PESO </h3> 
+                    The Public Employment Service Office (PESO) is a multi-service facility designed to offer employment information and assistance to clients 
+                    of the Department of Labor and Employment (DOLE) and the constituents of Local Government Units (LGUs). PESO consolidates various employment 
+                    promotion programs and services from DOLE and other government agencies, making it easier for all types of clientele to access information 
+                    and seek the specific assistance they need.
+                </div>
+                <div class="block mb-2">
+                    <h2>Objectives of PESO</h2>
+                    <div class="row mb-3 text-center">
+                        <div class="col-6">
+                            <div class="obejective">
+                                <div><br><h3>General Objectives<h3></div>
+                                <div>Ensure the prompt, timely and efficient delivery of employment service and provision of information on the other DOLE programs.</div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="obejective">
+                                <div><br><h3>Specific Objectives<h3></div>
+                                
+                                    <ul class="a">
+                                        <li>Provide a venue where people could explore simultaneously various employment options and actually seek assistance they prefer;</li>
+                                        <li>Serve as referral and information center for the various services and programs of DOLE and other government agencies present in the area;</li>
+                                        <li>Provide clients with adequate information on employment and labor market situation in the area; and</li>
+                                        <li>Network with other PESOs within the region on employment for job exchange purposes.</li>
+                                    </ul>
+                                
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="block">
+                    <h3>Organizatinal Outcome</h3>
+                        <ul class="b">
+                            <li>Full employment opportunities for all</li>
+                            <li>Capable and empowered citizenry through skills training</li>
+                            <li>Efficient OFW and Migration Development Center</li>
+                            <li>Enterprise Community through incubation and livelihood development</li>
+                        </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
