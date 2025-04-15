6c7c37066614d7a6bde517ebf93e04f8ad04db70
Magenta21 <regeniw341@mcatag.com>
2025-04-15 10:20:20 +0800

    15/04/25

--- employer/employer_login.php
+++ employer/employer_login.php
@@ -8,13 +8,13 @@
     <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
-<div class="header">
-        <div class="container-fluid d-flex align-items-center">
-            <a href="../index.php" class="back-button me-auto">
-            ← Back
-            </a>
-        </div>
-    </div>
+        <div class="header">
+                <div class="container-fluid d-flex align-items-center">
+                    <a href="../index.php" class="back-button me-auto">
+                    ← Back
+                    </a>
+                </div>
+            </div>
        
     <div class="container-fluid d-flex justify-content-center align-items-center mt-5">
         <div class="row front-box">
