aaac09b67068d5956e4206b983aed49afe026673
Magenta21 <regeniw341@mcatag.com>
2025-04-29 15:48:39 +0800

    29/4/25

--- ofw/ofw_login.php
+++ ofw/ofw_login.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['user'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Employer Login - Municipality of Los Banos</title>
+    <title>OFW Login - Municipality of Los Banos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/login.css">
 </head>
